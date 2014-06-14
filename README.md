ed442e0252a8ab53fa33dd6f94c456e62f15a929
ufo22940268 <ufo22940268@gmail.com>
2014-06-14 15:40:27 +0800

    up

---
+++ README.md
@@ -0,0 +1,21 @@
+### 友盟脱裤
+
+作为一名运营，你是否还在苦恼于没法查看应用的友盟渠道
+作为一只程序员，你是否还在想查看渠道名的时候发现自己的电脑上刚好没有apktool
+
+没有关系，**友盟脱裤**能够为你轻松查看手机上所有应用的友盟渠道名
+
+##screenshot
+
+![](./screenshot.png)
+
+##Play store
+
+<a href="https://play.google.com/store/apps/details?id= me.biubiubiu.umengpantsoff">
+  <img alt="Get it on Google Play"
+       src="/images/brand/en_generic_rgb_wo_60.png" />
+</a>
+
+##网盘
+
+[link](http://pan.baidu.com/s/1eQtJilO)
---
+++ app/src/main/java/me/biubiubiu/umengpantsoff/MyActivity.java
@@ -0,0 +1,129 @@
+package me.biubiubiu.umengpantsoff;
+
+import android.app.ListActivity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MyActivity extends ListActivity implements AdapterView.OnItemClickListener {
+
+    private List<AppItem> mAppItems;
+    private AppsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new AppsAdapter();
+        getListView().setAdapter(mAdapter);
+        getListView().setOnItemClickListener(this);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        getListView().setItemChecked(position, true);
+        AppItem appItem = mAppItems.get(position);
+        Toast.makeText(this, "友盟渠道名为" + appItem.umengChannel, Toast.LENGTH_SHORT).show();
+    }
+
+    class AppItem {
+
+        public String name;
+        public Drawable applicationIcon;
+        public String umengChannel;
+
+        @Override
+        public String toString() {
+            return name + "\t" + umengChannel;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PackageManager packageManager = this.getPackageManager();
+        List<ApplicationInfo> installedApplications = packageManager.getInstalledApplications(PackageManager.GET_ACTIVITIES | PackageManager.GET_META_DATA);
+        mAppItems = new ArrayList<AppItem>();
+        for (ApplicationInfo app : installedApplications) {
+            AppItem appItem = new AppItem();
+            appItem.name = (String) packageManager.getApplicationLabel(app);
+            appItem.applicationIcon = packageManager.getApplicationIcon(app);
+            if (app.metaData != null && app.metaData.get("UMENG_CHANNEL") != null) {
+                appItem.umengChannel = app.metaData.getString("UMENG_CHANNEL");
+                mAppItems.add(appItem);
+            }
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+    class AppsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            if (mAppItems == null) {
+                return 0;
+            } else {
+                return mAppItems.size();
+            }
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = getLayoutInflater().inflate(R.layout.list_item_app, parent, false);
+            }
+
+            AppItem item = mAppItems.get(position);
+            ((ImageView)view.findViewById(R.id.icon)).setImageDrawable(item.applicationIcon);
+            ((TextView)view.findViewById(R.id.label)).setText(item.name);
+            return view;
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.my, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+}
